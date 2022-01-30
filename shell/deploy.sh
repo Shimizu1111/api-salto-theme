9e8eef160e123480e8f53acc7cd2b91aa8647daf
net2katsu <net2katsu1507@yahoo.co.jp>
2022-01-30 18:33:07 +0900

    ciの修正16

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -4,8 +4,9 @@
 # ssh -o "StrictHostKeyChecking no" -i "../shmizu1111.pem" ec2-user@13.115.122.171
 # ssh -i "../shmizu1111.pem" ec2-user@13.115.122.171 git -C api-salto-theme status
 
+
 # ファイル転送(ローカルからリモート)
-scp -i "../shmizu1111.pem" ./hello.txt ec2-user@13.115.122.171:~/api-salto-theme/cmd
+scp  ./hello.txt ec2-user@13.115.122.171:~/api-salto-theme/cmd
 # scp -i "../shmizu1111.pem" ./main ec2-user@13.115.122.171:~/api-salto-theme/main
 
 # ファイル転送(リモートからリモート)
