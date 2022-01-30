386df4e274571cea1a733e81d0623a43b63f8c40
net2katsu <net2katsu1507@yahoo.co.jp>
2022-01-30 17:22:45 +0900

    ci修正10

---
+++ shell/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# @以降にAWSのIPアドレスを記載する
+# ssh -o "StrictHostKeyChecking no" -i "../shmizu1111.pem" ec2-user@13.115.122.171
+ssh -i "../shmizu1111.pem" ec2-user@13.115.122.171 git -C api-salto-theme status
+
+# ファイル転送(ローカルからリモート)
+# scp -i "../shmizu1111.pem" ./main ec2-user@13.115.122.171:~/api-salto-theme/main
+
+# ファイル転送(リモートからリモート)
+# scp user1@192.168.10.1:/home/user/tmp/file1 user2@192.168.10.2:/home/user/tmp/
+
+# ssh -i "../shmizu1111.pem" ec2-user@13.115.122.171 pwd
+# ssh -i "../shmizu1111.pem" ec2-user@13.115.122.171 pwd
+# ssh -i "../shmizu1111.pem" ec2-user@13.115.122.171 pwd
+# exit
